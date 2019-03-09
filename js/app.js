448d14ec0c9424e58cee00a985c3ab21bc569948
Emanuel <emanuel.j.acosta@gmail.com>
2019-03-09 19:22:58 -0300

    correccion display

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function calculadora() {
   // Variables
-  var resultado= document.getElementById("display");
+  var display= document.getElementById("display");
   var on= document.getElementById("on");
   var sign= document.getElementById("sign");
   var punto= document.getElementById("punto");
@@ -22,95 +22,146 @@ function calculadora() {
   var operando1;
   var operando2;
   var operacion;
+  var resultado = "";
 
   //Eventos
   num0.onclick= function (e) {
-    if (resultado.textContent="0") {
-      resultado.textContent= "0"
-    }else {
-      resultado.textContent = resultado.textContent + "0"
-    }
+    resultado.textContent = resultado.textContent + "0"
+    num0.style="padding:1%"
+    setInterval(resetNum, 400)
   }
   num1.onclick= function (e) {
-    if (resultado.textContent="0") {
-      resultado.textContent= "1"
-    }else {
-      resultado.textContent = resultado.textContent + "1"
-    }
+    display.textContent = resultado + "1"
+    resultado= display.textContent
+    num1.style="padding:1%"
+    setInterval(resetNum, 400)
   }
   num2.onclick= function (e) {
-    resultado.textContent = resultado.textContent + "2"
+    display.textContent = resultado + "2"
+    resultado= display.textContent
+    num2.style="padding:1%"
+    setInterval(resetNum, 400)
   }
   num3.onclick= function (e) {
     resultado.textContent = resultado.textContent + "3"
+    num3.style="padding:1%"
+    setInterval(resetNum, 400)
   }
   num4.onclick= function (e) {
     resultado.textContent = resultado.textContent + "4"
+    num4.style="padding:1%"
+    setInterval(resetNum, 400)
   }
   num5.onclick= function (e) {
     resultado.textContent = resultado.textContent + "5"
+    num5.style="padding:1%"
+    setInterval(resetNum, 400)
   }
   num6.onclick= function (e) {
     resultado.textContent = resultado.textContent + "6"
+    num6.style="padding:1%"
+    setInterval(resetNum, 400)
   }
   num7.onclick= function (e) {
     resultado.textContent = resultado.textContent + "7"
+    num7.style="padding:1%"
+    setInterval(resetNum, 400)
   }
   num8.onclick= function (e) {
     resultado.textContent = resultado.textContent + "8"
+    num8.style="padding:1%"
+    setInterval(resetNum, 400)
   }
   num9.onclick= function (e) {
     resultado.textContent = resultado.textContent + "9"
+    num9.style="padding:1%"
+    setInterval(resetNum, 400)
   }
   ////////////////////////////////////////////////////
   mas.onclick= function (e) {
     operando1= resultado.textContent;
     operacion= "+";
     limpiar();
+    mas.style="padding:1%";
+    setInterval(resetNum, 400);
   }
   menos.onclick= function (e) {
     operando1= resultado.textContent;
     operacion= "-";
     limpiar();
+    menos.style="padding:1%";
+    setInterval(resetNum, 400);
   }
   dividido.onclick= function (e) {
     operando1= resultado.textContent;
     operacion= "/";
     limpiar();
+    dividido.style="padding:1%";
+    setInterval(resetNum, 400);
   }
   por.onclick= function (e) {
     operando1= resultado.textContent;
     operacion= "*";
     limpiar();
+    por.style="padding:1%";
+    setInterval(resetNum, 400);
   }
   ////////////////////////////////////////////////////
   on.onclick= function (e) {
     reset();
+    on.style="padding:1%";
+    setInterval(resetNum, 400);
   }
   igual.onclick= function (e) {
     operando2= resultado.textContent;
     resolver();
+    igual.style="padding:1%";
+    setInterval(resetNum, 400);
   }
   sign.onclick= function (e) {
     resultado.textContent = "-" + resultado.textContent
+    sign.style="padding:1%";
+    setInterval(resetNum, 400);
   }
   punto.onclick= function (e) {
-    if (resultado.textContent=".") {
-
-    }
     resultado.textContent = resultado.textContent + "."
+    punto.style="padding:1%";
+    setInterval(resetNum, 400);
   }
 
   //Funciones
-  function limpiar() {
-    resultado.textContent= "0";
+  function resetNum() {
+    num0.style="padding:0"
+    num1.style="padding:0"
+    num2.style="padding:0"
+    num3.style="padding:0"
+    num4.style="padding:0"
+    num5.style="padding:0"
+    num6.style="padding:0"
+    num7.style="padding:0"
+    num8.style="padding:0"
+    num9.style="padding:0"
+    mas.style="padding:0"
+    menos.style="padding:0"
+    dividido.style="padding:0"
+    por.style="padding:0"
+    on.style="padding:0"
+    igual.style="padding:0"
+    sign.style="padding:0"
+    punto.style="padding:0"
   }
+
+  function limpiar() {
+    resultado.textContent= "";
+  }
+
   function reset() {
     resultado.textContent= "0";
     operando1= 0;
     operando2= 0;
     operacion= "";
   }
+
   function resolver() {
     var res= 0;
     switch (operacion) {
